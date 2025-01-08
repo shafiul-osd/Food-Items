8765617e5d54f4c7c5112325b7ddcb116fd5cbac
shafiul-osd <mdshafiulislamsafi2506@gmail.com>
2025-01-08 23:39:14 +0600

    adding some style for banner section

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -12,9 +12,26 @@ const Banner = () => {
           backgroundPosition: "top",
           backgroundRepeat: "no-repeat",
         }}
-        className="rounded-xl"
+        className="rounded-xl  flex items-center justify-center mt-10"
       >
-        <h2>hellow bg </h2>
+        <div className="text-white text-center  px-6">
+          <h2 className="text-6xl font-bold">
+            Discover an exceptional cooking class tailored for you!
+          </h2>
+          <p className="font-bold  px-10">
+            Learn and Master Basic Programming, Data Structures, Algorithm, OOP,
+            Database and solve 500+ coding problems to become an exceptionally
+            well world-class Programmer.
+          </p>
+          <div className="flex gap-4 items-center justify-center mt-6">
+            <button className="bg-purple-600 p-2 rounded-2xl">
+              Explore Now
+            </button>
+            <button className="border border-white p-2 rounded-2xl">
+              Our Feedback
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
